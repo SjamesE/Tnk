396ef7da28d2c5fc7cfbd2621d005e1dfa9112a9
sjame <sjame@DESKTOP-JK8G8QR>
2022-01-10 23:15:36 +0100

    Add project files.

---
+++ Tnk/Time.cs
@@ -0,0 +1,27 @@
+﻿using SFML.System;
+
+namespace Tnk
+{
+    internal class Time
+    {
+        private Clock clock;
+
+        public float deltaTime { get; private set; }
+        public float totalTime { get; private set; }
+
+        private float lastTime = 0;
+
+        Time()
+        {
+            clock = new Clock();
+        }
+
+        public void Update()
+        {
+            totalTime = clock.ElapsedTime.AsSeconds();
+            deltaTime = totalTime - lastTime;
+            lastTime = totalTime;
+        }
+
+    }
+}
