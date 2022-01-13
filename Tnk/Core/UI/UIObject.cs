6519b91f7eb97b113f2249108072d27502c70125
sJames <sjameselvis@gmail.com>
2022-01-13 23:31:39 +0100

    Ui

---
+++ Tnk/Core/UI/UIObject.cs
@@ -0,0 +1,12 @@
+﻿using Tnk.Generics;
+using Tnk.Core;
+
+namespace Tnk.Core.UI
+{
+    internal abstract class UIObject
+    {
+        public Transform transform { get; private set; }
+
+
+    }
+}
